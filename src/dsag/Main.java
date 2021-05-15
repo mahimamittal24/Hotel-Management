480cad3788b9e7cb1e66b498c35faa7e2550b523
mahima <mahimamittal829@gmail.com>
2021-05-15 10:55:35 +0530

    Hotel_Management

---
+++ src/dsag/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dsag;
+
+/**
+ *
+ * @author mukesh
+ */
+public class Main {
+ public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new login().setVisible(true);
+            }
+        });
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    
+
+    
+
+}
